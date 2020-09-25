f0a9fc8aa50d3ecf2852b02c01979ee54afb5b01
Furmanova <40866860+Furmanova@users.noreply.github.com>
2020-09-25 19:42:04 +0200

    сreate load balancer and backend addon

--- LoadBalancer/src/com/telran/Main.java
+++ LoadBalancer/src/com/telran/Main.java
@@ -1,27 +1,30 @@
 package com.telran;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import com.telran.backend.IBackendServerStorage;
+import com.telran.backend.SetBackendStorage;
+import com.telran.task.GatewayUdpSender;
+import com.telran.task.BackendUdpListener;
 
 public class Main {
+    // the port which listens to Backends
+   static final int UDP_BACKEND_LISTENER_PORT = 3000;
+   static final String GATEWAY_HOST = "localhost";
+    // coordinates of Gateway
+   static final int UDP_GATEWAY_PORT = 5001;
+   static final int SENDING_PERIOD_MILLIS = 100;
 
-    static final int SERVER_POT_BACKEND = 4002;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+            IBackendServerStorage storage = new SetBackendStorage();
 
-        ExecutorService pool = Executors.newFixedThreadPool(10);
-        ServerSocket serverSocket = new ServerSocket(SERVER_POT_BACKEND);
+            // start listening load data from the backends
+            BackendUdpListener backendUdpListener = new BackendUdpListener(storage, UDP_BACKEND_LISTENER_PORT);
+            Thread listenerThread = new Thread(backendUdpListener);
+            listenerThread.start();
 
-        UDPServerBackend udpThread = new UDPServerBackend();
-        pool.execute(udpThread);
-
-        while (true) {
-            // сервер принимает соединение
-            Socket socket = serverSocket.accept();
-            Runnable gatewayThread = new UDPClientGateway(socket, udpThread);
-            pool.execute(gatewayThread);
+            // start periodically send optimal com.tel-ran.backend data to the Gateway
+            GatewayUdpSender gatewaySender = new GatewayUdpSender(GATEWAY_HOST, UDP_GATEWAY_PORT, storage, SENDING_PERIOD_MILLIS);
+            Thread senderThread = new Thread(gatewaySender);
+            senderThread.start();
         }
-    }
 }
